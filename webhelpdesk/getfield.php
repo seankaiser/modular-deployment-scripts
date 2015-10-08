888726fd1ea80bc8595fb994c7246e0538d72539
David Davidson <g00271860@gmit.ie>
2015-10-09 00:02:01 +0100

    Fix remote code execution #1
    Fix remote code execution vulnerability caused by passing unsanitized user inputs to a shell.

--- webhelpdesk/getfield.php
+++ webhelpdesk/getfield.php
@@ -1,6 +1,6 @@
 <?php
 
-	$output=shell_exec('/usr/local/bin/whdapi.sh ' . $_GET['field'] . ' ' . 'mac-address' . ' ' . $_GET['mac_address']);
+	$output=shell_exec('/usr/local/bin/whdapi.sh ' . escapeshellarg($_GET['field']) . ' ' . 'mac-address' . ' ' . escapeshellarg($_GET['mac_address']));
 	echo $output;
 #}
 
